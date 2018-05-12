82643d84dc12b8d59a2edaba511556fa9cf063e0
lizhengquan1985 <entergl@163.com>
2018-05-12 11:19:12 +0800

    1

---
+++ DogGogo/DogApi/Controller/ControlController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DogApi.Controller
+{
+    public class ControlController : ApiController
+    {
+        public async Task Update(string symbolName, int )
+        {
+
+        }
+
+
+    }
+}

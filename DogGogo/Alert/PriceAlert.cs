6e50b2d4b2b4f952724f40d5a5af056a9f37dc25
lizhengquan1985 <entergl@163.com>
2018-05-25 22:00:17 +0800

    alert

---
+++ DogGogo/Alert/PriceAlert.cs
@@ -0,0 +1,53 @@
+﻿using DogPlatform;
+using DogPlatform.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Alert
+{
+    class PriceAlert
+    {
+        public static void RunAlert(CommonSymbols symbol, decimal big, decimal small)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+
+                    try
+                    {
+
+                        PlatformApi api = PlatformApi.GetInstance("xx");
+                        var period = "1min";
+                        var klines = api.GetHistoryKline(symbol.BaseCurrency + symbol.QuoteCurrency, period);
+
+                        var nowPrice = klines[0].Close;
+                        Console.WriteLine(klines[0].Close);
+
+                        if(nowPrice > big)
+                        {
+
+                            System.Diagnostics.Process.Start("explorer.exe", "http://blog.csdn.net/testcs_dn");
+                        }
+
+                        if(nowPrice < small)
+                        {
+                            System.Diagnostics.Process.Start("explorer.exe", "http://blog.csdn.net/testcs_dn");
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    Thread.Sleep(1000 * 5);
+                }
+            });
+
+        }
+    }
+}
---
+++ DogGogo/Alert/Program.cs
@@ -0,0 +1,32 @@
+﻿using DogPlatform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var symbols = CoinUtils.GetAllCommonSymbols();
+            System.Diagnostics.Process.Start("explorer.exe", "http://blog.csdn.net/testcs_dn");
+            while (true)
+            {
+                Console.WriteLine("请输入symbol");
+                var symbol = Console.ReadLine();
+                Console.WriteLine("请输入big");
+                var big = Console.ReadLine();
+                Console.WriteLine("请输入small");
+                var small = Console.ReadLine();
+
+                PriceAlert.RunAlert(symbols.Find(it=>it.BaseCurrency == symbol), decimal.Parse(big), decimal.Parse(small));
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}

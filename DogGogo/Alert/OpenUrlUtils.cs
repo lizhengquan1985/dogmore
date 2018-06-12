35482198cdf03359649e71680c6a65124ae189db
lizhengquan1985 <entergl@163.com>
2018-06-12 17:22:32 +0800

    闹钟

---
+++ DogGogo/Alert/OpenUrlUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alert
+{
+    public class OpenUrlUtils
+    {
+        private static DateTime dt = DateTime.Now;
+        public static void Open()
+        {
+            if ((DateTime.Now - dt).TotalSeconds > 60)
+            {
+                System.Diagnostics.Process.Start("explorer.exe", "http://blog.csdn.net/testcs_dn");
+                dt = DateTime.Now;
+            }
+        }
+    }
+}

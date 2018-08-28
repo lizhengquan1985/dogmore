b5624219c4733713fe2d1cd608004e5213b63192
lizhengquan1985 <entergl@163.com>
2018-08-28 21:09:36 +0800

    1

--- DogGogo/DogApi/Controller/MoreController.cs
+++ DogGogo/DogApi/Controller/MoreController.cs
@@ -83,7 +83,7 @@ namespace DogApi.Controller
                         var todayList = historyKlineData.Data.Where(it => Utils.GetDateById(it.Id) >= DateTime.Now.Date).Select(it => it).ToList();
                         todayDic.Add(symbol.BaseCurrency, todayList.Max(it => it.Close) / todayList.Min(it => it.Close));
                         todayDic.Add(symbol.BaseCurrency+"-", close / todayList.Min(it => it.Close));
-                        todayDic.Add(symbol.BaseCurrency + "+", close / todayList.Max(it => it.Close));
+                        todayDic.Add(symbol.BaseCurrency + "+", todayList.Max(it => it.Close) / close);
                     }
                     catch (Exception ex)
                     {
@@ -102,7 +102,7 @@ namespace DogApi.Controller
                 var todayList = historyKlineData.Data.Where(it => Utils.GetDateById(it.Id) >= DateTime.Now.Date).Select(it => it).ToList();
                 todayDic.Add(symbolName, todayList.Max(it => it.Close) / todayList.Min(it => it.Close));
                 todayDic.Add(symbolName + "-", close / todayList.Min(it => it.Close));
-                todayDic.Add(symbolName + "+", close / todayList.Max(it => it.Close));
+                todayDic.Add(symbolName + "+", todayList.Max(it => it.Close) / close);
             }
 
             Dictionary<string, decimal> ladderDic = new Dictionary<string, decimal>();

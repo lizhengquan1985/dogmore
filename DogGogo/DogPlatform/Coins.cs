70f6749612f2236319d2ecbd11cdbceb6a150ddf
lizhengquan1985 <entergl@163.com>
2018-10-19 19:01:50 +0800

    1

--- DogGogo/DogPlatform/Coins.cs
+++ DogGogo/DogPlatform/Coins.cs
@@ -9,51 +9,89 @@ namespace DogPlatform
 {
     public class CoinUtils
     {
-        private static Dictionary<string, CommonSymbols> coins = new Dictionary<string, CommonSymbols>();
+        private static Dictionary<string, CommonSymbols> usdtCoins = new Dictionary<string, CommonSymbols>();
+        private static Dictionary<string, CommonSymbols> btcCoins = new Dictionary<string, CommonSymbols>();
+        private static Dictionary<string, CommonSymbols> ethCoins = new Dictionary<string, CommonSymbols>();
+        private static Dictionary<string, CommonSymbols> htCoins = new Dictionary<string, CommonSymbols>();
 
         public static void Init()
         {
             PlatformApi api = PlatformApi.GetInstance("xx"); // 不需要角色,可以随意xx
             var commonSymbols = api.GetCommonSymbols();
-            commonSymbols = commonSymbols.FindAll(it => it.QuoteCurrency == "usdt");
-            foreach (var item in commonSymbols)
+
+            // usdt
+            var usdtCommonSymbols = commonSymbols.FindAll(it => it.QuoteCurrency == "usdt");
+            foreach (var item in usdtCommonSymbols)
             {
                 item.LeastBuyQuantity = 1; // TODO 改进
-                if (!coins.ContainsKey(item.BaseCurrency))
+                if (!usdtCoins.ContainsKey(item.BaseCurrency))
                 {
-                    coins.Add(item.BaseCurrency, item);
+                    usdtCoins.Add(item.BaseCurrency, item);
+                }
+            }
+
+            // b
+            var btcCommonSymbols = commonSymbols.FindAll(it => it.QuoteCurrency == "btc");
+            foreach (var item in btcCommonSymbols)
+            {
+                item.LeastBuyQuantity = 1; // TODO 改进
+                if (!btcCoins.ContainsKey(item.BaseCurrency))
+                {
+                    btcCoins.Add(item.BaseCurrency, item);
+                }
+            }
+
+            // e
+            var ethCommonSymbols = commonSymbols.FindAll(it => it.QuoteCurrency == "eth");
+            foreach (var item in ethCommonSymbols)
+            {
+                item.LeastBuyQuantity = 1; // TODO 改进
+                if (!ethCoins.ContainsKey(item.BaseCurrency))
+                {
+                    ethCoins.Add(item.BaseCurrency, item);
+                }
+            }
+
+            // ht
+            var htCommonSymbols = commonSymbols.FindAll(it => it.QuoteCurrency == "ht");
+            foreach (var item in htCommonSymbols)
+            {
+                item.LeastBuyQuantity = 1; // TODO 改进
+                if (!htCoins.ContainsKey(item.BaseCurrency))
+                {
+                    htCoins.Add(item.BaseCurrency, item);
                 }
             }
         }
 
-        public static CommonSymbols Get(string name)
-        {
-            if (!coins.ContainsKey(name))
-            {
-                Init();
-            }
+        //public static CommonSymbols Get(string name)
+        //{
+        //    if (!coins.ContainsKey(name))
+        //    {
+        //        Init();
+        //    }
 
-            return coins[name];
-        }
+        //    return coins[name];
+        //}
 
-        public static List<string> GetAllCoins()
-        {
-            // 总共其实有36个, 后期还会增加
-            if (coins.Count < 10)
-            {
-                Init();
-            }
-            return coins.Keys.ToList();
-        }
+        //public static List<string> GetAllCoins()
+        //{
+        //    // 总共其实有36个, 后期还会增加
+        //    if (coins.Count < 10)
+        //    {
+        //        Init();
+        //    }
+        //    return coins.Keys.ToList();
+        //}
 
         public static List<CommonSymbols> GetAllCommonSymbols()
         {
             // 总共其实有36个, 后期还会增加
-            if (coins.Count < 30)
+            if (usdtCoins.Count < 30)
             {
                 Init();
             }
-            return coins.Values.ToList();
+            return usdtCoins.Values.ToList();
         }
     }
 }
---
+++ DogGogo/RestMarket/Program.cs
@@ -0,0 +1,26 @@
+﻿using DogPlatform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoinUtils.Init();
+
+            Console.ReadLine();
+        }
+
+
+        public static void a()
+        {
+            // 准备好各种对
+
+        }
+    }
+}

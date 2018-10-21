37fa0639acd1778d039f4642a89482fce9d059a4
lizhengquan1985 <entergl@163.com>
2018-10-21 23:07:46 +0800

    111

---
+++ DogGogo/AutoTrade/Program.cs
@@ -0,0 +1,37 @@
+﻿using DogPlatform;
+using DogRunService;
+using DogRunService.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BeginTrade();
+        }
+
+        private static void BeginTrade()
+        {
+            // 初始化
+            CoinUtils.Init();
+
+            UserPools.Push("xx");
+            UserPools.Push("qq");
+
+            // 初始化k线
+            //KlineUtils.Begin();
+
+            // 不停的对每个币做操作
+            BuyOrSellUtils.Begin();
+
+            // 状态检查
+            TradeStateUtils.Begin();
+        }
+    }
+}

4957838be2259de7ff2a0da7470bae2ca0fe827c
174474866@qq.com <174474866@qq.com>
2019-03-04 11:37:00 +0800

    1

--- DogGogo/DogApi/Controller/ControlController.cs
+++ DogGogo/DogApi/Controller/ControlController.cs
@@ -99,19 +99,19 @@ namespace DogApi.Controller
                     }
                     if (pre50.Contains(item.SymbolName))
                     {
-                        item.Memo = pre50.IndexOf(item.SymbolName) < 20 ? "前20 -- 推荐 4level" : "前40 -- 推荐 6level";
+                        item.Memo = pre50.IndexOf(item.SymbolName) < 20 ? "前20 -- 推荐 2level" : "前40 -- 推荐 4level";
                     }
                     else if (pre80.Contains(item.SymbolName))
                     {
-                        item.Memo = pre80.IndexOf(item.SymbolName) < 20 ? "前60 -- 推荐 8level" : "前80 -- 推荐 10level";
+                        item.Memo = pre80.IndexOf(item.SymbolName) < 20 ? "前60 -- 推荐 6level" : "前80 -- 推荐 8level";
                     }
                     else if (pre120.Contains(item.SymbolName))
                     {
-                        item.Memo = pre120.IndexOf(item.SymbolName) < 20 ? "前100 -- 推荐 12level" : "前120 -- 推荐 14level";
+                        item.Memo = pre120.IndexOf(item.SymbolName) < 20 ? "前100 -- 推荐 10level" : "前120 -- 推荐 12level";
                     }
                     else
                     {
-                        item.Memo = "120开外 -- 推荐 16level";
+                        item.Memo = "120开外 -- 推荐 14level";
                     }
 
                     item.Memo += $" --------------- {levelData}";
